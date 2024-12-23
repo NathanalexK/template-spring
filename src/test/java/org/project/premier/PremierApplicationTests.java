42c953f719964b9cc93f53bd6984454b12527e5e
NathanalexK <miarynathan@hotmail.com>
2024-12-23 06:26:55 +0300

    Initial commit

---
+++ src/test/java/org/project/premier/PremierApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.premier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
